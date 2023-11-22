34bd7bcc28ae1c2f74acf15323daab37b6da2b8a
solomon kassa <118729276+Solomonkassa@users.noreply.github.com>
2023-11-22 13:25:34 +0300

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author solomon kassa
  */
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   fs.readFile(dataPath, 'utf-8', (err, data) => {
